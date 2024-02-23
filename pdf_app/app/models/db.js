e7eb6aaddfdef67e07a045405d7e235b22f77f0c
Hugo <cieplucha.hugo@gmail.com>
2024-02-23 16:47:59 +0100

    started using .env file

--- pdf_app/app/models/db.js
+++ pdf_app/app/models/db.js
@@ -3,10 +3,10 @@ const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.HOST,
+  user: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE
 });
 
 // open the MySQL connection
